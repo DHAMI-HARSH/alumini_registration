ccb17f918cc0cf143e08a2eb049fc914ea17cfe2
DHAMI-HARSH <158556321+DHAMI-HARSH@users.noreply.github.com>
2025-04-11 19:30:55 +0530

    king7

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
-import { ThemeProvider } from "@/components/ui/theme/theme-provider"
+import { ThemeProvider } from "next-themes"
 
 const inter = Inter({ subsets: ["latin"] })
 
